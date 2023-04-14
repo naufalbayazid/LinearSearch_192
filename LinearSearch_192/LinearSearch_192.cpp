d6b7213156ff45bbdef5b41835c50076566968f9
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-04-14 10:50:07 +0700

    enter element array

--- LinearSearch_192/LinearSearch_192.cpp
+++ LinearSearch_192/LinearSearch_192.cpp
@@ -10,11 +10,22 @@ int i;
 void input()
 {
 	while (true)
-
+	{
 		cout << "Enter the number of elements in the array: ";
-	cin >> n;
-	if ((n > 0) && (n <= 20))
-		break;
-	else
-		cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+	}	cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+
+
+
+	cout << "\n-----------------------\n";
+	cout << "Enter array elements \n";
+	cout << "--------------------\n;
+		for (i = 0; i < n; i++)
+		{
+			cout << "<" << (i + 1) << "> ";
+			cin >> arr[i];
+		}
 }
