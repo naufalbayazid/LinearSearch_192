6bcd5589540e6c757e7c2c7dd8df806e513a72ff
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-04-14 11:00:31 +0700

    mencari nilai n

--- LinearSearch_192/LinearSearch_192.cpp
+++ LinearSearch_192/LinearSearch_192.cpp
@@ -58,6 +58,12 @@ void LinearSearch()
 			cout << "\n" << item << " not found in the array\n";
 		cout << "\nNumber of comparisons: " << comparison << endl;
 
+		cout << "\nContinue search  (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'Y'));
+}
+
+
 		
 
 			
